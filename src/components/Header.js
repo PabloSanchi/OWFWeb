d92690170e2dc2ae47f374a941538079b1b684ae
Pablo <pablo.sanchi.herrera@gmail.com>
2023-03-24 21:28:18 +0100

    add framer motion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import { motion } from "framer-motion"
 
 const Header = () => {
   return (
     <header className='select-none shadow-lg p-5'>
         <div className='flex items-center justify-between max-w-[60rem] mx-auto'>
-            <div>
+            <motion.div
+            initial={{x: -500, opacity: 0, scale: 0.5}}
+            animate={{x: 0, opacity: 1, scale: 1}}
+            transition={{duration: 0.5}}>
                 <h1 className='text-xl lg:text-3xl font-bold'>MetricSIM</h1>
                 <h2 className='text-lg lg:text-xl font-light'>Data display</h2>
-            </div>
+            </motion.div>
 
-            <button className='text-lg lg:text-xl font-light border-2 border-gray-500 rounded-md px-4 py-2 hover:bg-gray-500 hover:text-white'>
+            <motion.button 
+            initial={{y: -50, opacity: 0, scale: 0.5}}
+            animate={{y: 0, opacity: 1, scale: 1}}
+            transition={{duration: 0.5}}
+            className='text-lg lg:text-xl font-light border-2 border-gray-500 rounded-md px-4 py-2 hover:bg-gray-500 hover:text-white'>
                 DOCS
-            </button>
+            </motion.button>
 
-            <div className='hidden md:flex space-x-4  items-center'>
+            <motion.div 
+            initial={{x: 500, opacity: 0, scale: 0.5}}
+            animate={{x: 0, opacity: 1, scale: 1}}
+            transition={{duration: 0.5}}
+            className='hidden md:flex space-x-4  items-center'>
                 <a href='#repo' rel='noreferrer' target='_blank'>
                     <GitHubIcon />
                 </a>
                 {/* <a href='#repo' rel='noreferrer' target='_blank'>
                     <PictureAsPdfIcon />
                 </a> */}
-            </div>
+            </motion.div>
 
         </div>
     </header>
